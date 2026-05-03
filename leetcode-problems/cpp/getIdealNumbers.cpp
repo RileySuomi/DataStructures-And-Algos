111c799edc3d0707c03055e154c3175eb36f3a05
RileySuomi <suomir@spu.edu>
2026-05-03 14:12:29 -0700

    ibm backend OA

---
+++ leetcode-problems/cpp/getIdealNumbers.cpp
@@ -0,0 +1,24 @@
+
+
+using namespace std;
+
+// dont have my solution copied as it was an OA
+
+// but basically giving a range low and high, we want to find the number of ideal numbers in that range
+// numbers that are divisible by both 3 and 5 *
+
+
+long getIdealNumbers(long low, long high) {
+
+   long result = 0;
+
+   for (long x = 1; x <= high; x *= 3) {
+       for (long y = x; y <= high; y *= 5) {
+           if (y >= low) {
+            result++;
+           }
+       }
+   }
+   return result;
+}
+
